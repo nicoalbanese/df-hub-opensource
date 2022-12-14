7312489decd8460be879a108015b51f3abedf1b2
Nico Albanese <gcalbanese96@gmail.com>
2022-12-14 18:04:08 +0000

    added base layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -12,7 +12,12 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        {/* We've used 3xl here, but feel free to try other max-widths based on your needs */}
+        <div className="mx-auto max-w-3xl pt-4">
+          <Component {...pageProps} />
+        </div>
+      </div>
     </SessionProvider>
   );
 };
